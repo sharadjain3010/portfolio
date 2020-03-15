3c6fb3011fb75506d2359057e34694d686890a5e
sjain <sharadk.jain>
2020-03-15 23:36:00 +0530

    Updates

---
+++ precache-manifest.1c3161eb173b0a41daa6934a6a7d27b9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1697aaa03553cab488dbb5e2f932259f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "092e1348fd6b81ee55f2",
+    "url": "/portfolio/static/css/main.ab23d7d8.chunk.css"
+  },
+  {
+    "revision": "0e31c16e06534dc1dc85",
+    "url": "/portfolio/static/js/2.76584c22.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.76584c22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "092e1348fd6b81ee55f2",
+    "url": "/portfolio/static/js/main.c9cd896f.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "388938f6332e5e07466341f141effc6c",
+    "url": "/portfolio/static/media/Roboto-Bold-webfont.388938f6.woff"
+  },
+  {
+    "revision": "654b91dd4a8fb0d6e09e659de1d8ab20",
+    "url": "/portfolio/static/media/Roboto-Bold-webfont.654b91dd.ttf"
+  },
+  {
+    "revision": "b67b57ee8df108bdf0af63b1feb48e1e",
+    "url": "/portfolio/static/media/Roboto-Bold-webfont.b67b57ee.eot"
+  },
+  {
+    "revision": "edfbc8c46197306fa7dcd0598009aa87",
+    "url": "/portfolio/static/media/Roboto-Bold-webfont.edfbc8c4.svg"
+  },
+  {
+    "revision": "60c8cca19deb18ab40beca55d9c31d06",
+    "url": "/portfolio/static/media/Roboto-Thin-webfont.60c8cca1.svg"
+  },
+  {
+    "revision": "7c8b8e72e9f6b040c113d74c61a16b2a",
+    "url": "/portfolio/static/media/Roboto-Thin-webfont.7c8b8e72.ttf"
+  },
+  {
+    "revision": "8e1161212ab2327118ae9276ec0e22a2",
+    "url": "/portfolio/static/media/Roboto-Thin-webfont.8e116121.eot"
+  },
+  {
+    "revision": "faf10f3c4744408e813b6958b6ca623b",
+    "url": "/portfolio/static/media/Roboto-Thin-webfont.faf10f3c.woff"
+  },
+  {
+    "revision": "dcce534c4b0bf4dfd6bc8a73eda1fe22",
+    "url": "/portfolio/static/media/userImage.dcce534c.jpg"
+  }
+]);
